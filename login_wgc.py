028a50e0271f64803ee3e3617aec04d40b907006
Jeferson Luis <jgjefersonluis@gmail.com>
2022-02-26 13:33:20 -0300

    inserindo a visualização dos buttons, labels e telas.

--- login_wgc.py
+++ login_wgc.py
@@ -11,5 +11,15 @@ def abre_tela_cadastro():
     tela_cadastro.show()
 
 
+app = QtWidgets.QApplication([])
+primeira_tela = uic.loadUi("primeira_tela.ui")
+segunda_tela = uic.loadUi("segunda_tela.ui")
+tela_cadastro = uic.loadUi("tela_cadastro.ui")
+primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
+segunda_tela.pushButton.clicked.connect(logout)
+primeira_tela.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+primeira_tela.pushButton_2.clicked.connect(abre_tela_cadastro)
+tela_cadastro.pushButton.clicked.connect(cadastrar)
 primeira_tela.show()
+
 app.exec()
